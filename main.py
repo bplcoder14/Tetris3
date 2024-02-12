d6ddb824920e6c2d98b18925f5fca35ee3728532
bplcoder14 <bplcoder14@gmail.com>
2024-02-12 11:29:59 -0600

    reset project to use pygame instead

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
-import pyglet
-from pyglet import shapes
-from pyglet.text import Label
+import pygame
+from pygame.locals import *
+import sys
 
-background = pyglet.graphics.Group(order=0)
-middle_ground_0 = pyglet.graphics.Group(order=1)
-middle_ground_1 = pyglet.graphics.Group(order=2)
-middle_ground_2 = pyglet.graphics.Group(order=3)
-foreground = pyglet.graphics.Group(order=4)
+pygame.init()
 
+# colors
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+CLEAR = (0, 0, 0, 0)
 
-class Button:
+FPS = 30
 
-    def __init__(self, x, y, width, height, border_size, color, border_color, text, batch):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.border_color = border_color
-        self.border_size = border_size
-        self.text = text
-        self.batch = batch
-        self.border_rectangle = shapes.Rectangle(self.x, self.y, self.width,
-                                                 self.height, color=self.border_color,
-                                                 group=middle_ground_0, batch=self.batch)
-        self.inner_rectangle = shapes.Rectangle(self.x, self.y, self.width - self.border_size,
-                                                self.height - self.border_size, color=self.color, group=middle_ground_1
-                                                , batch=self.batch)
-        self.label = Label(text=self.text, x=self.x, y=self.y, width=self.width, height=self.height, multiline=True,
-                           group=middle_ground_2, batch=self.batch, anchor_x='center', anchor_y='center')
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 1000
 
-    def set_display(self):
-        pass
+win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('TETRIS')
+clock = pygame.time.Clock()
 
 
-class MainWindow(pyglet.window.Window):
+def main():
+    while True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        win.fill(BLACK)
+        pygame.display.flip()
 
-    def __init__(self):
-        super().__init__(width=1000, height=800, caption='Tetris')
-        self.batch = pyglet.graphics.Batch()
-        self.buttons = [Button(500, 400, 100, 75, 3, (255, 0, 0), (255, 255, 255), "button 1", self.batch)]
 
-    def on_draw(self):
-        self.clear()
-        self.batch.draw()
 
 
 if __name__ == '__main__':
-    window = MainWindow()
-    pyglet.app.run()
+    main()
